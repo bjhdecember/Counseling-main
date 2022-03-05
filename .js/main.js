87fb19fb2e1d650e638dca547ad420912dd1b094
bjhdecember <barbarajh1202@yahoo.com>
2022-03-05 17:50:09 -0500

    changes

---
+++ .js/main.js
@@ -0,0 +1,39 @@
+const input = document.querySelector('#name');
+const input = document.querySelector('#email');
+const input = document.querySelector('#feedback');
+const form = docmument.querySelector('#form');
+const submit = document.querySelector('#submitName');
+
+var storage = sessionStorage;
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
+
+submit.addEventListener('click', () => {
+    storage.setItem('name', input.value);
+    updateNameTitle();
+});
+
+submit.addEventListener('click', () => {
+    storage.setItem('email', input.value);
+    updateEmailTitle();
+});
+
+remove.addEventListener('click', () => {
+    storage.removeItem('name'); 
+    updateNameTitle();
+});
+
+remove.addEventListener('click', () => {
+    storage.removeItem('email'); 
+    updateEmailTitle();
+});
+
+function updateNameTitle() {
+    const name = storage.getItem('name');
+
+    const h1 = document.querySelector('#title');
+
+    name ? h1.textContent = 'Welcome ${name}' : h1.textContent = '';
+}
--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+// Bring in the express server and create application
+let express = require('express')
+let app = express();
+
+// use the express
+let router = express.Router();
+
+// Create Get to return a list of all charts
+router.get('/', function (req, res, next) {
+  res.send("chart");
+});
+
+// Configure router so all routes are prefixed with /api/vl
+app.use('/api/', router);
+
+// Create server to listen port 5000
+var server = app.listen(5500, function () {
+  console.log('Node server is running on http://localhost:5500..');
+});
+
 require('dotenv').config()
 const express = require('express')
 
@@ -8,3 +28,4 @@ server.get('/hello', (req, res) => {
 })
 
 server.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}!`))
+
