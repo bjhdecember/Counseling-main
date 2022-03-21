a84ad72f1428cf94902d91f6d70793fc3d8873cc
bjhdecember <barbarajh1202@yahoo.com>
2022-03-21 19:44:02 -0400

    changes

--- repos/chartRepo.js
+++ repos/chartRepo.js
@@ -1,15 +1,13 @@
 let fs = require("fs");
 //const { resolve } = require("path/posix");
 
-const FILE_NAME = './assets/charts.json';
+const FILE_NAME = "./assets/charts.json";
 
 let chartRepo = {
     post: function (resolve, reject) {
         fs.readFile(FILE_NAME, function (err, data) {
             if (err) {
-
-            }
-            else {
+            } else {
                 resolve(JSON.parse(data));
             }
         });
@@ -18,19 +16,17 @@ let chartRepo = {
         fs.readFile(FILE_NAME, function (err, data) {
             if (err) {
                 reject(err);
-            }
-            else {
-                let chart = JSON.parse(data).find(p => p.id == id);
+            } else {
+                let chart = JSON.parse(data).find((p) => p.id == id);
                 resolve(chart);
             }
-        })
+        });
     },
     search: function (searchObject, resolve, reject) {
         fs.readFile(FILE_NAME, function (err, data) {
             if (err) {
                 reject(err);
-            }
-            else {
+            } else {
                 let charts = JSON.parse(data);
                 // Preform search
                 if (searchObject) {
@@ -40,8 +36,14 @@ let chartRepo = {
                     // "name": 'A'
                     // };
                     charts = charts.filter(
-                        p => (searchObject.id ? p.id == searchObject.id : true) &&
-                            (searchObject.name ? p.name.toLowerCase().indexOf(searchObject.name.toLowerCase()) >= 0 : true));
+                        (p) =>
+                            (searchObject.id ? p.id == searchObject.id : true) &&
+                            (searchObject.name
+                                ? p.name
+                                    .toLowerCase()
+                                    .indexOf(searchObject.name.toLowerCase()) >= 0
+                                : true)
+                    );
                 }
 
                 resolve(charts);
@@ -52,40 +54,38 @@ let chartRepo = {
         fs.readFile(FILE_NAME, function (err, data) {
             if (err) {
                 reject(err);
-            }
-            else {
+            } else {
                 let charts = JSON.parse(data);
                 charts.push(newData);
                 fs.writerFile(FILE_NAME, JSON.stringify(charts), function (err) {
                     if (err) {
                         reject(err);
                     }
-                })
-            }
-        })
-    },
-    update: function (newData, id, resolve, reject) {
-    fs.readFile(FILE_NAME, function (err, data) {
-        if (err) {
-            reject(err);
-        }
-        else {
-            let charts = JSON.parse(data);
-            let chart = charts.find(p => p.id == id);
-            if (chart); {
-                Object.assign(chart, newData);
-                fs.writeFile(FILE_NAME, JSON.stringify(charts), function (err) {
-                    if (err) {
-                        reject(err);
-                    }
-                    else {
-                        resolve(newData);
-                    }
                 });
             }
-        }
-    });
-}
-}
+        });
+    },
+    update: function (newData, id, resolve, reject) {
+        fs.readFile(FILE_NAME, function (err, data) {
+            if (err) {
+                reject(err);
+            } else {
+                let charts = JSON.parse(data);
+                let chart = charts.find((p) => p.id == id);
+                if (chart);
+                {
+                    Object.assign(chart, newData);
+                    fs.writeFile(FILE_NAME, JSON.stringify(charts), function (err) {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve(newData);
+                        }
+                    });
+                }
+            }
+        });
+    },
+};
 
 module.exports = chartRepo;
